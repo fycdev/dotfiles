3f6e4c1357af8178aee0984a9831a9fbefdddbe9
Franky Cheung <27916006+fycdev@users.noreply.github.com>
2023-02-22 15:42:40 +1100

    update fish with tmux layout command and clean up

---
+++ fish/functions/tdev.fish
@@ -0,0 +1,46 @@
+function tdev
+  if test -n "$TMUX"
+    echo 'Error: Currently in a tmux environment.'
+    return 1
+  end
+
+  argparse 'n/name=' 'p/path=' -- $argv
+  or return
+
+  if set -ql _flag_p
+    if not test -d $_flag_p
+      echo 'Error: Path not found.'
+      return 1
+    end
+
+    set -f path $_flag_p
+  else
+    set -f path $PWD
+  end
+
+  if set -ql _flag_n
+    set -f name $_flag_n
+  else
+    set -f name (basename $PWD)
+  end
+
+  tmux has -t $name
+  
+  if test $status -ne 0
+    tmux new -d -s $name -c $PWD
+
+    if test $status -eq 0
+      tmux renamew -t $name:1 'editor'
+      tmux neww -t $name -n 'shell'
+      tmux splitw -t $name:2
+      tmux splitw -t $name:2
+      tmux splitw -t $name:2
+      tmux selectl -t $name:2 tiled
+      tmux selectp -t $name:2.1
+      tmux selectw -t $name:1
+      tmux send -t $name:1.1 'nvim' C-m
+    end
+  end
+
+  tmux a -t $name
+end
