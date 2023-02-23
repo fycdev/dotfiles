bfd25031a7e1306623d32d4346777fedf98cc99e
Franky Cheung <27916006+fycdev@users.noreply.github.com>
2023-02-24 00:56:51 +1100

    update git status symbols in prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -8,14 +8,16 @@ function fish_prompt
   set -q fish_prompt_pwd_dir_length
   or set -lx fish_prompt_pwd_dir_length 0
 
-  set -g __fish_git_prompt_showuntrackedfiles 1
   set -g __fish_git_prompt_showdirtystate 1
-  set -g __fish_git_prompt_showupstream 'informative'
-
+  set -g __fish_git_prompt_showuntrackedfiles 1
+  set -g __fish_git_prompt_showupstream 'auto'
   set -g __fish_git_prompt_char_dirtystate !
-  set -g __fish_git_prompt_char_invalidstate =
+  set -g __fish_git_prompt_char_invalidstate ~
   set -g __fish_git_prompt_char_stagedstate +
   set -g __fish_git_prompt_char_untrackedfiles ?
+  set -g __fish_git_prompt_char_upstream_ahead ⇡
+  set -g __fish_git_prompt_char_upstream_behind ⇣
+  set -g __fish_git_prompt_char_upstream_diverged ⇕
 
   set -l suffix '❯'
   if functions -q fish_is_root_user; and fish_is_root_user
