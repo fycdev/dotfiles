97be57b06d8a138b5f006d439a7da096ab4e16f9
Franky Cheung <27916006+fycdev@users.noreply.github.com>
2023-02-24 18:01:26 +1100

    update fish prompt git symbols

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -16,10 +16,13 @@ function fish_prompt
   set -g __fish_git_prompt_char_invalidstate 
   set -g __fish_git_prompt_char_stagedstate 
   set -g __fish_git_prompt_char_untrackedfiles 
-  set -g __fish_git_prompt_char_upstream_ahead ⇡
-  set -g __fish_git_prompt_char_upstream_behind ⇣
-  set -g __fish_git_prompt_char_upstream_diverged ⇕
+  set -g __fish_git_prompt_char_upstream_ahead 
+  set -g __fish_git_prompt_char_upstream_behind 
+  set -g __fish_git_prompt_char_upstream_diverged 
   set -g __fish_git_prompt_char_upstream_equal ""
+  set -g __fish_git_prompt_char_upstream_prefix " "
+
+  set -l trimmed_git_prompt (string replace -r -a '[\(\)]' '' (fish_vcs_prompt))
 
   set -l suffix '❯'
   if functions -q fish_is_root_user; and fish_is_root_user
@@ -30,6 +33,6 @@ function fish_prompt
       set status_color (set_color brred)
   end
 
-  echo -s $cwd_color (prompt_pwd) $vcs_color (fish_vcs_prompt) ' '
+  echo -s $cwd_color (prompt_pwd) $vcs_color $trimmed_git_prompt
   echo -n -s $status_color $suffix ' ' $normal
 end
