5aa7fbee12599282303c5c0f3cf20ccc4c65eba6
Franky Cheung <27916006+fycdev@users.noreply.github.com>
2023-05-30 11:47:16 +1000

    add performance options for neovim loading

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,21 +6,34 @@ require("keymaps")
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins", {
-  defaults = { lazy = false },
-  checker = { enabled = true },
-  change_detection = { notify = false },
+	defaults = { lazy = false },
+	checker = { enabled = true },
+	change_detection = { notify = false },
+	performance = {
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				"matchit",
+				"matchparen",
+				"netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
 })
-
