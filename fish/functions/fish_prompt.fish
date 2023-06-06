2a8bf3eee54f6d847222fffc9772e255cfbaa184
Franky Cheung <27916006+fycdev@users.noreply.github.com>
2023-06-06 14:43:04 +1000

    fix git prompt icons in fish

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -14,7 +14,7 @@ function fish_prompt
   set -g __fish_git_prompt_showupstream 'auto'
   set -g __fish_git_prompt_char_dirtystate 
   set -g __fish_git_prompt_char_invalidstate 
-  set -g __fish_git_prompt_char_stagedstate 
+  set -g __fish_git_prompt_char_stagedstate 
   set -g __fish_git_prompt_char_untrackedfiles 
   set -g __fish_git_prompt_char_upstream_ahead 
   set -g __fish_git_prompt_char_upstream_behind 
